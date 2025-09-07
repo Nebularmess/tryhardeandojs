af600f1fbb3f3d337f291aad0d9b92810b04f262
Nicolás Soria <105177329+Nebularmess@users.noreply.github.com>
2025-09-07 16:16:40 -0300

    iniciando-bucles

---
+++ Introduccion/06Bucles/Bucles.js
@@ -0,0 +1,12 @@
+//Estructuras de control para manejar flujo de codigo en bloques
+// for ejecutarse segun lo lanzamos, si es tru sigue ejecutando
+arreglo = [1,2,3,4,5]
+for(let i = 0; i < arreglo.length; i++){
+    console.log(arreglo[i])
+}
+//while
+let i = 0;
+while (i < arreglo.length) {
+    console.log (arreglo[i])
+    i++
+}
