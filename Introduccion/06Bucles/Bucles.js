194752daac5c0a647373f612d89e4d89b85c686c
nicolas <sorianicolas3786@gmail.com>
2025-09-10 17:34:21 -0300

    resolviendo-ciclos

--- Introduccion/06Bucles/Bucles.js
+++ Introduccion/06Bucles/Bucles.js
@@ -10,3 +10,25 @@ while (i < arreglo.length) {
     console.log (arreglo[i])
     i++
 }
+// do while
+i = 0;
+do {
+     console.log (arreglo[i] + 1)
+    i++
+} while (i < arreglo.length);
+mySet = new Set(["nico", "maxim", "soria", 27]);
+myMap = new Map([[0, "nico"],[1, 12], [2, true], [3, "soria"]]);
+// for of
+for (let valor of mySet) {
+    console.log(valor)
+};
+for (let [clave, valor] of myMap) {
+    console.log(clave, valor)
+};
+myString = "Hola Mundo";
+for (let letra of myString) {
+    console.log(letra)
+};
+//break y continue
+//continue, el bucle continua a la siguiente iteracion
+//break termina el bucle
---
+++ Introduccion/06Bucles/ejercicios.js
@@ -0,0 +1,50 @@
+/*
+Clase 30 - Ejercicios: Bucles
+Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
+*/
+
+// NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
+
+// 1. Crea un bucle que imprima los números del 1 al 20
+for (i = 0; i < 20; i++) {
+    console.log(i+1)
+}
+// 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
+let suma = 0;
+for (i =0; i < 100; i++) {
+    suma = suma + (i+1)
+    if (i === 99) {
+        console.log (suma)
+    }
+}
+
+// 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+for (i=1; i <= 50; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+// 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+let arreglo= ["nico", "maxi", "simon", "juampi"]
+for (nombre of arreglo) {
+    console.log(nombre)
+}
+// 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+let string = "opaopaopa"
+// 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+        let suma2 = 0;
+for (letra of string){
+    if (letra == "o" || letra == "a" || letra == "e" || letra == "i" || letra == "u") {
+        suma2 = suma2 +1;
+    }
+}
+console.log (" tiene " + suma2+ " vocales")
+// 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+for (i = 0; i <= 10; i++) {
+    console.log(i + " x 5 = " + (i*5))
+}
+// 8. Usa un bucle para invertir una cadena de texto
+
+// 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
+
+// 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
