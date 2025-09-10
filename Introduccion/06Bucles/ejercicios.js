23c514857647cb5040087e122534a4cafbf90a23
Nicolás Soria <105177329+Nebularmess@users.noreply.github.com>
2025-09-10 18:39:04 -0300

    resueltobucles

--- Introduccion/06Bucles/ejercicios.js
+++ Introduccion/06Bucles/ejercicios.js
@@ -30,7 +30,7 @@ for (nombre of arreglo) {
     console.log(nombre)
 }
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
-let string = "opaopaopa"
+let string = "camus hacker"
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
         let suma2 = 0;
 for (letra of string){
@@ -44,7 +44,47 @@ for (i = 0; i <= 10; i++) {
     console.log(i + " x 5 = " + (i*5))
 }
 // 8. Usa un bucle para invertir una cadena de texto
-
+let stringInvertido = "";
+for (i = string.length; i > 0; i--) {
+    console.log(string[i - 1 ])
+    stringInvertido = stringInvertido + string[i - 1] //rekcah sumac
+}
+console.log(" es: " + stringInvertido)
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
+let numero = [];
+for(i = 0; i < 10; i++){  
+    if (i == 0){
+        numero [0] = 0;
+        console.log(numero[i])
+    } else if ( i == 1) {
+        numero[i] = 1
+        console.log(numero[i]) 
+    } else {
+        numero[i] = numero[i - 1] + numero [i -2];
+        console.log(numero[i]) 
+    }
+}
+numero = [];
+for (i = 0; i< 10; i++) {
+    if(numero[0] == undefined) {
+        numero[i] = 0;
+        console.log(numero[i])
+        continue
+    }
+    if(numero[i-1] == undefined || numero[i-2]==undefined) {
+        numero[i] = 1
+        console.log(numero[i])
+        continue
+    }
+    numero[i] = numero[i-1] + numero[i-2]
+    console.log(numero[i])
+}
 
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+let numerosMayores =[];
+for(numeros of numero) {
+    if(numeros > 10) {
+        numerosMayores.push(numeros)
+    }
+}
+console.log(numerosMayores)
